4af9e103e370367482ffbac892c9fa9e0b66ed9e
meidinarevisp <revimeidina72@gmail.com>
2024-05-01 11:29:50 +0700

    Initialize restaurant catalogue automated testing implemented

--- src/scripts/components/page-index/RestaurantList.js
+++ src/scripts/components/page-index/RestaurantList.js
@@ -1,104 +1,72 @@
-import 'lazysizes'
-import 'lazysizes/plugins/parent-fit/ls.parent-fit'
-import './SearchRestaurant'
+import "lazysizes";
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
+import "./SearchRestaurant";
 
 class RestaurantList extends HTMLElement {
   constructor() {
-    super()
-    this._restaurants = null
-    this._isLoading = true
+    super();
   }
 
   connectedCallback() {
-    this.render()
+    this.render();
   }
 
   set restaurants(restaurants) {
-    this._restaurants = restaurants
-    this._isLoading = false
-    this.render()
+    this._restaurants = restaurants;
+    this.render();
   }
 
   render() {
-    if (this._isLoading) {
-      this.renderSkeletonUI()
-    } else {
-      this.renderRestaurants()
-    }
-  }
+    if (!this._restaurants) return;
 
-  renderSkeletonUI() {
     this.innerHTML = `
-      <div class="explore-restaurant" id="explore-content">
+    <div class="explore-restaurant" id="explore-content">
         <h2>Explore Restaurant</h2>
         <p>Discover Great Dining Experience...</p>
       </div>
       <search-bar></search-bar>
       <section class="restaurants">
-        <div class="restaurant-skeleton">
-          <div class="image"></div>
-          <div class="name"></div>
-          <div class="description"></div>
-        </div>
-        <div class="restaurant-skeleton">
-          <div class="image"></div>
-          <div class="name"></div>
-          <div class="description"></div>
-        </div>
-        <!-- Tambahkan lebih banyak elemen skeleton sesuai kebutuhan -->
+        ${this._restaurants.map(this.renderRestaurant).join("")}
       </section>
-    `
-  }
-
-  renderRestaurants() {
-    this.innerHTML = `
-      <div class="explore-restaurant" id="explore-content">
-        <h2>Explore Restaurant</h2>
-        <p>Discover Great Dining Experience...</p>
-      </div>
-      <search-bar></search-bar>
-      <section class="restaurants">
-        ${this._restaurants.map(this.renderRestaurant).join('')}
-      </section>
-    `
+    `;
   }
 
   renderRestaurant(restaurant) {
     const truncatedDescription =
-      restaurant.description.substring(0, 100) + '...'
+      restaurant.description.substring(0, 100) + "...";
 
     return `
       <div class="restaurant">
         <img
           class="lazyload"
           data-src="https://restaurant-api.dicoding.dev/images/small/${
-  restaurant.pictureId
-}"
+            restaurant.pictureId
+          }"
           alt="${restaurant.name}">
         <a href="./detail.html?id=${restaurant.id}" class="restaurant-name">${
-  restaurant.name
-}</a>
+      restaurant.name
+    }</a>
         <h3 class="star-rating">${
-  generateStarRating(restaurant.rating).stars
-} (${generateStarRating(restaurant.rating).numericRating})</h3>
+          generateStarRating(restaurant.rating).stars
+        } (${generateStarRating(restaurant.rating).numericRating})</h3>
         <p>${truncatedDescription}</p>
         <h4>${restaurant.city}</h4>
       </div>
-    `
+    `;
   }
 }
 
 function generateStarRating(rating) {
-  const fullStars = Math.floor(rating)
-  const halfStars = Math.ceil(rating - fullStars)
-  const emptyStars = 5 - fullStars - halfStars
+  const fullStars = Math.floor(rating);
+  const halfStars = Math.ceil(rating - fullStars);
+  const emptyStars = 5 - fullStars - halfStars;
 
   const stars =
-    '★'.repeat(fullStars) + '☆'.repeat(halfStars) + '☆'.repeat(emptyStars)
+    "★".repeat(fullStars) + "☆".repeat(halfStars) + "☆".repeat(emptyStars);
 
-  const numericRating = rating.toFixed(1)
+  const numericRating = rating.toFixed(1);
 
-  return { stars, numericRating }
+  return { stars, numericRating };
 }
 
-customElements.define('restaurant-list', RestaurantList)
+customElements.define("restaurant-list", RestaurantList);
